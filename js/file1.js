fa5baff8a990988f14d2780f5c19801d759711a4
kashish-08 <kashhh204@gmail.com>
2025-06-20 10:03:18 +0530

    js folder

---
+++ js/file1.js
@@ -0,0 +1,27 @@
+let std_name = 'kashish'
+let age = 21
+let score
+let city = null
+let isStd = true
+console.log(typeof std_name)
+console.log(typeof age)
+console.log(typeof score)
+console.log(typeof city)
+console.log(typeof isStd)
+
+let strAge = String(age)
+console.log(typeof strAge)
+let rating = "5"
+console.log(typeof rating)
+let strRating = Number(rating)
+console.log(strRating)
+console.log(typeof strRating)
+
+let flag = "true"
+console.log(typeof flag)
+let boolFlag = Boolean(flag)
+console.log(typeof boolFlag)
+
+
+
+
---
+++ js/file2.js
@@ -0,0 +1,35 @@
+// let a = "10"
+// let b = 10
+// console.log(a+b)
+// console.log(a*b) 
+//100 becoz string me o multiply operator toh "10" treated as number..
+
+// let x = 2
+// let y = 2
+// console.log(x==y) //compares value
+
+// let x = "2"
+// let y = 2
+// console.log(x===y) //compares type
+
+// let x = 5
+// let y = 2
+// console.log(x!==y)
+
+// let a = 10
+// let b = 2
+// let c = 20
+// console.log(a>b && a>c)
+
+// let a = 10
+// let b = 2
+// let c = 20
+// console.log(a>b || a>c)
+
+// let a = 10
+// let b = 200
+// console.log(!(a>b)) //inner bracket must
+
+let a = 10
+let b = 2
+console.log(a**b)
